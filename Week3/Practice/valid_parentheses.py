22fbb564b9a78bb1f6927b8bfa0bd36222d6d11f
taeho <dlxogh439@gmail.com>
2024-09-06 22:31:19 +0900

    valid_parentheses 문제 풀이

--- Week3/Practice/valid_parentheses.py
+++ Week3/Practice/valid_parentheses.py
@@ -1,29 +1,42 @@
-from collections import Counter
-
 def isValid(s: str) -> bool:
     if len(s) == 1:
-            return False
-        
+        return False
+    
     if len(s) % 2 != 0:
+        return False    
+    
+    s_list = list(s)
+    stack = []
+    for char in s_list:
+        if len(stack) == 0:
+            stack.append(char)
+            continue
+        
+        latest_char = stack.pop()
+        if char == ')' and latest_char == '(':
+            continue
+        elif char == ']' and latest_char == '[':
+            continue
+        elif char == '}' and latest_char == '{':
+            continue
+        else:
+            stack.append(latest_char)            
+            stack.append(char)
+        
+    if len(stack) != 0:
         return False
 
-    s_counter = Counter(s)
-    parenthesis_count = s_counter['('] - s_counter[')']
-    braces_count = s_counter['{'] - s_counter['}']
-    brackets_count = s_counter['['] - s_counter[']']
-    
-    return ((parenthesis_count or braces_count or brackets_count) == 0)    
-    
+    return True
 
 # true
 # s = "()"
 # isValid(s)
 # true
 # s = "()[]{}"
-# isValid(s)
+# print(isValid(s))
 # false
-s = "((]"
-isValid(s)
-# true
-# s = "([])"
+# s = "((]"
 # isValid(s)
+# true
+s = "([])"
+isValid(s)
