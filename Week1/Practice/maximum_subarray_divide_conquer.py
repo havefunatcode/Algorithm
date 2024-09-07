e286266f2234db5f51eb2791d6c64d1321614b6f
thlee <thlee@wert.co.kr>
2024-09-07 10:04:47 +0900

    2개의 스택을 이용하여 하나의 큐를 만들기

--- Week1/Practice/maximum_subarray_divide_conquer.py
+++ Week1/Practice/maximum_subarray_divide_conquer.py
@@ -8,10 +8,16 @@ def maxSubArray(nums: list[int]) -> int:
         mid = (left + right) // 2
         left_sum = right_sum = cur_sum = 0
         
+        # 좌측의 최대 합
+        # 중간 지점 바로 앞에서 왼쪽 끝까지 역순으로 계산
+        # [1, -3, 4, -2, 2] 배열의 경우에 계산에 순서에 따라 최댓값이 변경 될 수 있다.
+        # 왼족 끝에서부터 중간까지 : 2
+        # 중간에서 왼쪽 끝까지 : 4
         for i in range(mid - 1, left - 1, -1):
             left_sum = max(left_sum, cur_sum := cur_sum + arr[i])
         
         cur_sum = 0
+        # 우측의 최대합
         for i in range(mid + 1, right):
             right_sum = max(right_sum, cur_sum := cur_sum + arr[i])
         
